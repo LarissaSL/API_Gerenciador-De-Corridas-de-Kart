ee6b419a1df0a6a1953a67e759e9b43ba5a383ec
LarissaSL <larissa.silvaedge@gmail.com>
2024-09-04 17:28:20 -0300

    BUILD - Flyway da tabela de corridas certo

--- src/main/resources/db/migration/V5__create-table-corridas.sql
+++ src/main/resources/db/migration/V5__create-table-corridas.sql
@@ -6,9 +6,10 @@ CREATE TABLE corridas (
     data DATE NOT NULL,
     horario TIME NOT NULL,
     transmissao BOOLEAN DEFAULT 0,
-    categoria ENUM('95', '110', 'Livre'),
+    categoria VARCHAR(20) DEFAULT 'LIVRE',
+    classificacao VARCHAR(20) NOT NULL,
     codigo VARCHAR(45) UNIQUE,
-    preco DECIMAL(10,2) CHECK (preco >= 0),
+    preco DECIMAL(10,2) CHECK (preco >= 0) NOT NULL,
     ativo TINYINT(1) DEFAULT 1,
     FOREIGN KEY (campeonato_id) REFERENCES campeonatos(id),
     FOREIGN KEY (kartodromo_id) REFERENCES kartodromos(id)
