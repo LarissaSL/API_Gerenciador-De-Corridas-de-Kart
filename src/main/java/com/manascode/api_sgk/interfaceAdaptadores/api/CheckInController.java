5fbfe1766bbc61a379b44332ecd57ce8f133ecd1
LarissaSL <larissa.silvaedge@gmail.com>
2024-09-30 15:49:48 -0300

    REFACTOR - Mudanca no codigo do Controller de Checkin e Corrida mapper

--- src/main/java/com/manascode/api_sgk/interfaceAdaptadores/api/CheckInController.java
+++ src/main/java/com/manascode/api_sgk/interfaceAdaptadores/api/CheckInController.java
@@ -1,15 +1,26 @@
 package com.manascode.api_sgk.interfaceAdaptadores.api;
 
-import com.manascode.api_sgk.aplicacao.check.*;
+import com.manascode.api_sgk.aplicacao.check.AtualizarCheckInDTO;
+import com.manascode.api_sgk.aplicacao.check.CheckService;
+import com.manascode.api_sgk.aplicacao.check.CriarCheckInDTO;
+import com.manascode.api_sgk.aplicacao.check.DetalharCheckDTO;
+import com.manascode.api_sgk.aplicacao.check.ListarCheckInDTO;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/check-in")
